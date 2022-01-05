21dac505fe9e8d286f3ffe7371e106e83f1f191f
Young Hwang <ykcul02@gmail.com>
2022-01-05 22:31:49 +0900

    feat: init commit

---
+++ src/main/java/io/ggammu/realquerydsl/RealQuerydslApplication.java
@@ -0,0 +1,13 @@
+package io.ggammu.realquerydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealQuerydslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealQuerydslApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/ggammu/realquerydsl/RealQuerydslApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ggammu.realquerydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealQuerydslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
