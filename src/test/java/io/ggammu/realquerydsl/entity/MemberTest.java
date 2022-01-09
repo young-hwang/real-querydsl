da68a6910e116befe5bcb470d82c61877cee4a9a
Young Hwang <ykcul02@gmail.com>
2022-01-09 19:40:25 +0900

    test: QueryDsl order 지정

--- src/test/java/io/ggammu/realquerydsl/entity/MemberTest.java
+++ src/test/java/io/ggammu/realquerydsl/entity/MemberTest.java
@@ -98,4 +98,29 @@ class MemberTest {
 
     }
 
+    /**
+     * 회원 정렬 순서
+     * 1. 회원 나이 내림차순(desc)
+     * 2. 회원 이름 오름차순(asc)
+     * 단 2에서 회원 이름이 없을 시 마지막에 출력(nulls last)
+     */
+    @Test
+    void sort() {
+        entityManager.persist(new Member(null, 100));
+        entityManager.persist(new Member("member5", 100));
+        entityManager.persist(new Member("member6", 100));
+
+        List<Member> result = query.selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+
+        Member member5 = result.get(0);
+        Member member6 = result.get(1);
+        Member memberNull = result.get(2);
+
+        assertThat(member5.getUsername()).isEqualTo("member5");
+        assertThat(member6.getUsername()).isEqualTo("member6");
+        assertThat(memberNull.getUsername()).isNull();
+    }
 }
