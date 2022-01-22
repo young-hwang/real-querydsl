7edf7e0728b3a3b2e2536ce7b03afb17eb23969d
Young Hwang <ykcul02@gmail.com>
2022-01-22 23:11:20 +0900

    feat: paging query 실행 controller 추가

--- src/main/java/io/ggammu/realquerydsl/controller/MemberController.java
+++ src/main/java/io/ggammu/realquerydsl/controller/MemberController.java
@@ -1,4 +1,36 @@
 package io.ggammu.realquerydsl.controller;
 
+import io.ggammu.realquerydsl.dto.MemberSearchCondition;
+import io.ggammu.realquerydsl.dto.MemberTeamDto;
+import io.ggammu.realquerydsl.repository.MemberJpaRepository;
+import io.ggammu.realquerydsl.repository.MemberRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
 public class MemberController {
+
+    private final MemberJpaRepository memberJpaRepository;
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/v1/members")
+    public List<MemberTeamDto> searchMemberV1(MemberSearchCondition condition) {
+        return memberJpaRepository.searchByWhereCondition(condition);
+    }
+
+    @GetMapping("/v2/members")
+    public Page<MemberTeamDto> searchMemberV2(MemberSearchCondition condition, Pageable pageable) {
+        return memberRepository.searchPageSimple(condition, pageable);
+    }
+
+    @GetMapping("/v3/members")
+    public Page<MemberTeamDto> searchMemberV3(MemberSearchCondition condition, Pageable pageable) {
+        return memberRepository.searchPageComplex(condition, pageable);
+    }
+
 }
